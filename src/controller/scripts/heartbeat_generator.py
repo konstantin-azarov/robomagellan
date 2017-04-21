df8561c2276dd22606003891ec263ca7ad14bd9a
Konstantin Azarov <konstantin.azarov@gmail.com>
2017-04-20 17:14:20 -0700

    Process remote control in heartbeat_generator

--- src/controller/scripts/heartbeat_generator.py
+++ src/controller/scripts/heartbeat_generator.py
@@ -2,16 +2,27 @@
 
 import rospy
 from std_msgs.msg import Empty
+from trex_dmc01.msg import Status
+
+g_active = False
+
+def status_callback(msg):
+    global g_active
+    g_active = (msg.state == 4 and msg.channels[1] > 4000)
 
 if __name__ == '__main__':
     rospy.init_node('heartbeat_generator')
+
+    force = rospy.get_param("~force", False)
     
     empty_msg = Empty()
 
+    rospy.Subscriber("/trex_dmc01/status", Status, status_callback)
     pub = rospy.Publisher('heartbeat', Empty, queue_size = 10)
     rate = rospy.Rate(20)
     
     while not rospy.is_shutdown():
-        pub.publish(empty_msg)
+        if force or g_active:
+            pub.publish(empty_msg)
         rate.sleep()
         
